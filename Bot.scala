5d9052ba47ff7575a13a82bd27c5e2b96dd25647
Denis Belov <aksion2011@yandex.ru>
2018-08-18 02:49:13 +0500

    Delete Bot.scala

--- Bot.scala
+++
@@ -1,9 +0,0 @@
-import info.mukel.telegrambot4s.api._
-import info.mukel.telegrambot4s.api.declarative.Commands
-
-import scala.io.Source
-
-object Bot extends TelegramBot with Polling with Commands{
-  lazy val token = Source.fromFile("src\\main\\scala\\bot.token").getLines().mkString
-  //onCommand('list) { implicit msg => reply(BotKernel.List(msg.from.get.id)) }
-}
